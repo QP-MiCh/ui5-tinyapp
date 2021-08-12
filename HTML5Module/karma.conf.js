819f6f0de885b577a28caba116a6be9371fb5eaf
Michael Christa <michael.christa@q-perior.com>
2021-08-12 10:21:34 +0000

    update karma config

--- HTML5Module/karma.conf.js
+++ HTML5Module/karma.conf.js
@@ -1,11 +1,16 @@
-const process = require('process');
-process.env.CHROME_BIN = require('puppeteer').executablePath();
 module.exports = function(config) {
     "use strict";
+    const process = require('process');
+    process.env.CHROME_BIN = require('puppeteer').executablePath();
+
     config.set({
         plugins: [
+            require('karma-jasmine'),
+            require('karma-chrome-launcher'),
+            require('karma-jasmine-html-reporter'),
+            require('karma-coverage-istanbul-reporter'),
             require('karma-coverage'),
-            require('karma-junit-reporter')            
+            require('karma-junit-reporter')
         ],
         frameworks: ["ui5"],
         browsers: ["ChromeHeadless"],
